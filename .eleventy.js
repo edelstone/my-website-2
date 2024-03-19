8ccaa9411265e1c0526eac61804c5642171201fe
Michael Edelstone <michael.edelstone@gmail.com>
2024-03-19 06:40:04 -0500

    tightens up when js is used

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./images");
   eleventyConfig.addPassthroughCopy("./fonts");
   eleventyConfig.addWatchTarget("./styles");
-  eleventyConfig.addPassthroughCopy("./scripts");
+  eleventyConfig.addPassthroughCopy("./js");
   eleventyConfig.addPassthroughCopy("*.svg");
   eleventyConfig.addPassthroughCopy("*.png");
   eleventyConfig.addPassthroughCopy("*.ico");
