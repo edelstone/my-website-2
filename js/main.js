28dcf9a509e411b4dbf622e0bd548492756a50de
Michael Edelstone <michael.edelstone@gmail.com>
2024-03-20 05:20:02 -0500

    updates images and other minor edits

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-// Add a class to the case study headers when scrolled
-let scrollHeader = window.scrollY
-const header = document.querySelector(".case-study-header")
-
-const addClass = () => header.classList.add("pinned")
-const removeClass = () => header.classList.remove("pinned")
-
-window.addEventListener('scroll', function () {
-  scrollHeader = window.scrollY;
-
-  if (scrollHeader >= 96) { addClass() }
-  else { removeClass() }
-
-  console.log(scrollHeader)
-})
+// Add a class to header when it becomes sticky
+const el = document.querySelector(".case-study-header");
+window.addEventListener("scroll", () => {
+  const stickyTop = parseInt(window.getComputedStyle(el).top);
+  const currentTop = el.getBoundingClientRect().top;
+  el.classList.toggle("pinned", currentTop === stickyTop);
+});
