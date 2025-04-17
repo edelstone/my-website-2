dba47db195d52e3fdb195aada2cf2b44cc225deb
Michael Edelstone <michael.edelstone@gmail.com>
2025-04-17 05:29:18 -0500

    adds passthrough values

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/work");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/*.svg");
   eleventyConfig.addPassthroughCopy("src/*.png");
-  eleventyConfig.addPassthroughCopy("src/*.jpg");
-  eleventyConfig.addPassthroughCopy("src/*.pdf");
   eleventyConfig.addPassthroughCopy("src/*.ico");
   eleventyConfig.addPassthroughCopy("src/*.webmanifest");
 
