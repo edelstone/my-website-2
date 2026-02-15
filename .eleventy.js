9967403ae6626046b7f2f06b321699b082117143
Michael Edelstone <michael.edelstone@gmail.com>
2026-02-15 01:10:20 -0600

    removes obsolete Eleventy passthrough config

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/work");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/*.svg");
   eleventyConfig.addPassthroughCopy("src/*.png");
   eleventyConfig.addPassthroughCopy("src/*.ico");
-  eleventyConfig.addPassthroughCopy("src/*.webmanifest");
   eleventyConfig.addPassthroughCopy('src/_redirects');
 
   eleventyConfig.setServerOptions({
